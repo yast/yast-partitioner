fd5d87120d4836e82af9875b1499e363f02309de
Josef Reidinger <jreidinger@suse.cz>
2017-05-04 09:31:47 +0200

    add list of icons from old storage

---
+++ src/lib/y2partitioner/icons.rb
@@ -0,0 +1,30 @@
+module Y2Partitioner
+  module Icons
+    ALL = "yast-disk.png"
+    HD = "yast-disk.png"
+    HD_PART = "yast-partitioning.png"
+
+    LVM = "yast-lvm_config.png"
+    LVM_LV = "yast-partitioning.png"
+
+    RAID = "yast-raid.png"
+
+    LOOP = "yast-encrypted.png"
+
+    DM = "yast-device-mapper.png"
+
+    NFS = "yast-nfs.png"
+
+    UNUSED = "yast-unused-device.png"
+
+    GRAPH = "yast-device-tree.png"
+
+    SUMMARY = "yast-disk.png"
+
+    SETTINGS = "yast-spanner.png"
+
+    LOG = "yast-messages.png"
+
+    ENCRYPTED = "yast-encrypted.png"
+  end
+end
