6d2a3d915099b08c7f775b359829426491512f57
Josef Reidinger <jreidinger@suse.cz>
2017-05-04 14:38:04 +0200

    add all items to overview

--- src/lib/y2partitioner/widgets/overview.rb
+++ src/lib/y2partitioner/widgets/overview.rb
@@ -15,7 +15,7 @@ module Y2Partitioner
     class Overview < CWM::CustomWidget
       def initialize
         self.handle_all_events = true
-        @opened = []
+        @opened = [:all]
       end
 
       def contents
@@ -38,14 +38,47 @@ module Y2Partitioner
             term(:icon, Icons::ALL),
             open?(:all),
             "machine", # TODO: stuck getting hostname on my pc Yast::Hostname.CurrentHostname,
-            subitems
+            machine_items
+          ),
+          # TODO: only if there is graph support UI.HasSpecialWidget(:Graph)
+          Item(
+            Id(:devicegraph),
+            term(:icon, Icons::GRAPH),
+            _("Device Graph"),
+            open?(:devicegraph)
+          ),
+          Item(
+            Id(:mountgraph),
+            term(:icon, Icons::GRAPH),
+            _("Mount Graph"),
+            open?(:mountgraph)
+          ),
+          Item(
+            Id(:summary),
+            term(:icon, Icons::SUMMARY),
+            _("Installation Summary"),
+            open?(:summary)
+          ),
+          Item(
+            Id(:settings),
+            term(:icon, Icons::SETTINGS),
+            _("Settings"),
+            open?(:settings)
           )
         ]
       end
 
-      def subitems
+      def machine_items
         [
-          harddisk_items
+          harddisk_items,
+          raid_items,
+          lvm_items,
+          crypt_files_items,
+          device_mapper_items,
+          nfs_items,
+          btrfs_items,
+          tmpfs_items,
+          unused_items
         ]
       end
 
@@ -56,6 +89,86 @@ module Y2Partitioner
           _("Hard Disks"),
           open?(:hd),
           [] # TODO: real disks subtree
+        )
+      end
+
+      def raid_items
+        Item(
+          Id(:raid),
+          term(:icon, Icons::RAID),
+          _("RAID"),
+          open?(:raid),
+          [] # TODO: real MD subtree
+        )
+      end
+
+      def lvm_items
+        Item(
+          Id(:lvm),
+          term(:icon, Icons::LVM),
+          _("Volume Management"),
+          open?(:lvm),
+          [] # TODO: real LVM subtree
+        )
+      end
+
+      def crypt_files_items
+        Item(
+          Id(:loop),
+          term(:icon, Icons::LOOP),
+          _("Crypt Files"),
+          open?(:loop),
+          [] # TODO: real subtree
+        )
+      end
+
+      def device_mapper_items
+        Item(
+          Id(:dm),
+          term(:icon, Icons::DM),
+          _("Device Mapper"),
+          open?(:dm),
+          [] # TODO: real subtree
+        )
+      end
+
+      def nfs_items
+        Item(
+          Id(:nfs),
+          term(:icon, Icons::NFS),
+          _("NFS"),
+          open?(:nfs),
+          [] # TODO: real subtree
+        )
+      end
+
+      def btrfs_items
+        Item(
+          Id(:btrfs),
+          term(:icon, Icons::NFS),
+          _("Btrfs"),
+          open?(:btrfs),
+          [] # TODO: real subtree
+        )
+      end
+
+      def tmpfs_items
+        Item(
+          Id(:tmpfs),
+          term(:icon, Icons::NFS),
+          _("tmpfs"),
+          open?(:tmpfs),
+          [] # TODO: real subtree
+        )
+      end
+
+      def unused_items
+        Item(
+          Id(:unused),
+          term(:icon, Icons::UNUSED),
+          _("Unused Devices"),
+          open?(:unused),
+          [] # TODO: real subtree
         )
       end
 
