64270c781f54565a844839192a9604a9c89b8b15
Martin Vidner <mvidner@suse.cz>
2017-06-02 17:28:42 +0200

    A dirty way to create a partition.

--- src/lib/y2partitioner/widgets/disk_page.rb
+++ src/lib/y2partitioner/widgets/disk_page.rb
@@ -66,22 +66,52 @@ module Y2Partitioner
       class AddTestingPartitionButton < CWM::PushButton
         # Y2Storage::Disk
         def initialize(disk)
+          textdomain "storage"
           @disk = disk
         end
 
         def label
-          "If there is no partition table, make a GPT table"
+          "Add an unformatted primary partition to this HD."
         end
 
-        def handle
-          pt = @disk.partition_table
-          p pt
-          if pt.nil?
-            type = Y2Storage::PartitionTables::Type.new("gpt")
-            @disk.create_partition_table(type)
+        # FIXME: stolen from Y2Storage::Proposal::PartitionCreator
+        # Make it DRY
+        def partition_table(disk)
+          disk.partition_table || disk.create_partition_table(disk.preferred_ptable_type)
+        end
+
+        # FIXME: stupid name
+        # @return [Y2Storage::PartitionTables::PartitionSlot,nil]
+        def free_slot_or_popup
+          pt = partition_table(@disk)
+          slots = pt.unused_partition_slots
+          if slots.empty?
+            Yast::Popup.Error(
+              Yast::Builtins.sformat(
+                _("It is not possible to create a partition on %1."),
+                @disk.name
+              )
+            )
+            return nil
           end
-          pt = @disk.partition_table
-          p pt
+          slots.first
+        end
+
+        # FIXME, we should (re)use Y2Storage::Proposal::PartitionCreator
+        def handle
+          pt = partition_table(@disk)
+          slot = free_slot_or_popup
+          return nil if slot.nil?
+          name = slot.name
+          # TODO: just make a small partition so that we can repeat this
+          # action without filling the whole empty space at once
+          region = slot.region
+          type = Y2Storage::PartitionType::PRIMARY
+          partition = pt.create_partition(name, region, type)
+          log.info "Created #{partition.inspect}"
+          # FIXME: tell the UI to show this new partition.
+          # switching tabs on this disk is not enough;
+          # switching to another disk and back helps
           nil
         end
       end
