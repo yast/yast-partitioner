02d2da1ce2e4fdeeefe1913e1152545dbb624fe7
Knut Alejandro Anderssen González <teclator@gmail.com>
2017-06-12 08:14:24 +0100

    Minimal transaction.

--- src/lib/y2partitioner/device_graphs.rb
+++ src/lib/y2partitioner/device_graphs.rb
@@ -16,32 +16,12 @@ module Y2Partitioner
     # @yieldreturn [Boolean]
     # @return What the block returned
     def transaction(&block)
-      old_dg = current
-      self.class.functional_transaction(old_dg) do |new_dg|
-        begin
-          self.current = new_dg
-          res = block.call
-          self.current = old_dg if !res
-          res
-        rescue
-          self.current = old_dg
-          raise
-        end
-      end
-    end
+      old_dg = current.dup
+      res = block.call
 
-    # Run a block with a **duplicate** of *old_dg*.
-    # If the block returns a truthy value,
-    # new_dg is **copy**ed to old_dg.
-    # @param old_dg [Devicegraph]
-    # @yieldparam new_dg, [Devicegraph]
-    # @yieldreturn [Boolean]
-    # @return What the block returned
-    def self.functional_transaction(old_dg, &block)
-      new_dg = old_dg.duplicate
-      accepted = block.call(new_dg)
-      new_dg.copy(old_dg) if accepted
-      accepted
+      self.current = old_dg if !res
+
+      res
     end
   end
 end
