206e9510ba3c0477035701c0006460723631cd14
Martin Vidner <mvidner@suse.cz>
2017-06-13 10:51:49 +0200

    Test fixups for create_instance

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -33,9 +33,6 @@ def devicegraph_stub(name)
   storage = Y2Storage::StorageManager.fake_from_yaml(path)
   storage.probed.copy(storage.staging)
 
-  dgs = Y2Partitioner::DeviceGraphs.instance
-  dgs.current = storage.staging
-  dgs.original = storage.probed
-
+  Y2Partitioner::DeviceGraphs.create_instance(storage.probed, storage.staging)
   storage
 end
